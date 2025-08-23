3d4504ed8bd3600b24a1742e3dbcb7f634b4731f
luisgustavo <luisgosampaio@gmail.com>
2025-08-23 20:14:18 -0300

    Hello World!

---
+++ src/main/java/com/gwent/gwent_api/GwentApiApplication.java
@@ -0,0 +1,13 @@
+package com.gwent.gwent_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwent/gwent_api/GwentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwent.gwent_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
