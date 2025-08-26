f487322ae68ed1602bdef9116cc679a381289947
luisgustavo <luisgosampaio@gmail.com>
2025-08-26 10:18:01 -0300

    feat: creating the skeleton of the "orchestrator" Game, for the main methods

--- gwent-engine/src/main/java/br.com.gwent.engine/Game.java
+++ gwent-engine/src/main/java/br.com.gwent.engine/Game.java
@@ -1,7 +1,25 @@
 package br.com.gwent.engine;
 
+import br.com.gwent.engine.pojo.structure.card.GameCard;
+
+import java.util.Deque;
+
 public class Game {
-    public String getStatus() {
-        return "Waiting for players...";
+    private GameState gameState;
+
+    public Game (Deque<GameCard> playerOneDeck, Long playerOneId, Deque<GameCard> playerTwoDeck, Long playerTwoId) {
+
+    }
+
+    public GameState playCard () {
+        return gameState;
+    }
+
+    public GameState passTurn () {
+        return gameState;
+    }
+
+    public GameState useLeaderCard () {
+        return gameState;
     }
 }
