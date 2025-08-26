6fca75a82470a7a30aa9dc3011012e9067d284f3
luisgustavo <luisgosampaio@gmail.com>
2025-08-26 09:55:57 -0300

    feat: implementing autoShuffle for the constructor on Player's class

--- gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/Player.java
+++ gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/Player.java
@@ -3,8 +3,7 @@ package br.com.gwent.engine.pojo.structure;
 import br.com.gwent.engine.pojo.structure.card.GameCard;
 import lombok.Data;
 
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 @Data
 public class Player {
@@ -12,4 +11,17 @@ public class Player {
     private boolean hasPassed = false;
     private List<GameCard> hand;
     private Deque<GameCard> deck;
+
+    public Player (Long userId, Deque<GameCard> deck) {
+        this.userId = userId;
+        this.deck = shuffleDeck(deck);
+    }
+
+    private Deque<GameCard> shuffleDeck (Deque<GameCard> notShuffledDeck) {
+        List<GameCard> listOfDeck = new ArrayList<>(notShuffledDeck);
+
+        Collections.shuffle(listOfDeck);
+
+        return new ArrayDeque<>(listOfDeck);
+    }
 }
