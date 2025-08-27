de31e3f2ae67a26f7e495f9ccf8eac3e0660b650
luisgustavo <luisgosampaio@gmail.com>
2025-08-27 11:08:15 -0300

    refac: changing the attribute points for being a method that retrieves all points of all cards, and added a method for adding cards to the row

--- gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/board/BoardRow.java
+++ gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/board/BoardRow.java
@@ -1,11 +1,23 @@
 package br.com.gwent.engine.pojo.structure.board;
 
 import br.com.gwent.engine.pojo.enums.RowType;
+import br.com.gwent.engine.pojo.structure.card.GameCard;
 import lombok.Builder;
 import lombok.Data;
 
-@Data @Builder
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class BoardRow {
     private final RowType rowType;
-    private int points;
+    private List<GameCard> cards = new ArrayList<>();
+
+    public int getTotalPower() {
+        return cards.stream().mapToInt(GameCard::getCurrentPower).sum();
+    }
+
+    public void addCardToRow (GameCard card) {
+        cards.add(card);
+    }
 }
