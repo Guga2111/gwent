12e6826c44456defbe0f7cbeb1e09f21d0a2551b
luisgustavo <luisgosampaio@gmail.com>
2025-08-27 11:12:14 -0300

    feat: adding a constructor for creating a rows for the playerBoard, a method for clearing the board (when the round is finished) and a method for getting the row based on the RowType

--- gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/board/PlayerBoard.java
+++ gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/board/PlayerBoard.java
@@ -1,6 +1,7 @@
 package br.com.gwent.engine.pojo.structure.board;
 
 import br.com.gwent.engine.pojo.enums.RowType;
+import br.com.gwent.engine.pojo.structure.Player;
 import lombok.Data;
 
 import java.util.EnumMap;
@@ -10,10 +11,27 @@ import java.util.Map;
 public class PlayerBoard {
     private Map<RowType, BoardRow> rows = new EnumMap<>(RowType.class);
 
+    public PlayerBoard () {
+        for(RowType type : RowType.values()) {
+            rows.put(type, new BoardRow(type));
+        }
+    }
+
     public int getTotalPoints () {
         return rows.values()
                 .stream()
-                .mapToInt(BoardRow::getPoints)
+                .mapToInt(BoardRow::getTotalPower)
                 .sum();
+    }
+
+    public void clearBoard (Player player) {
+        for(BoardRow row : rows.values()) {
+            player.getDiscard().addAll(row.getCards());
+            row.getCards().clear();
+        }
+    }
+
+    public BoardRow getRow (RowType type) {
+        return rows.get(type);
     }
 }
