264e235510293091f226ab1d57b4e134d3c6c817
luisgustavo <luisgosampaio@gmail.com>
2025-08-27 11:10:58 -0300

    feat: adding the board attribute for Player, and adding a method for retrieving the card and removing it from the player's hand

--- gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/Player.java
+++ gwent-engine/src/main/java/br/com/gwent/engine/pojo/structure/Player.java
@@ -1,5 +1,6 @@
 package br.com.gwent.engine.pojo.structure;
 
+import br.com.gwent.engine.pojo.structure.board.PlayerBoard;
 import br.com.gwent.engine.pojo.structure.card.GameCard;
 import lombok.Data;
 
@@ -8,7 +9,12 @@ import java.util.*;
 @Data
 public class Player {
     private final Long userId;
+
+    private int roundsWon = 0;
     private boolean hasPassed = false;
+
+    private PlayerBoard board;
+
     private List<GameCard> hand;
     private Deque<GameCard> deck;
     private List<GameCard> discard;
@@ -17,7 +23,17 @@ public class Player {
         this.userId = userId;
         this.deck = deck;
         this.hand = hand;
+
         this.discard = new ArrayList<>();
+        this.board = new PlayerBoard();
     }
 
+    public GameCard removeCardFromHand (UUID gameCardId) {
+        Optional<GameCard> requestCard = this.hand.stream()
+                .filter(card -> card.getInstanceId().equals(gameCardId))
+                .findFirst();
+
+        requestCard.ifPresent(card -> this.hand.remove(card));
+        return requestCard.get();
+    }
 }
