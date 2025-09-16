92a71f2a225f0e55ddb7770baa4ed1c25ae6a364
luisgustavo <luisgosampaio@gmail.com>
2025-09-16 11:29:31 -0300

    fix: refac little things, such as unnecessary exception

--- gwent-engine/src/main/java/br.com.gwent.engine/services/validator/GameValidator.java
+++ gwent-engine/src/main/java/br.com.gwent.engine/services/validator/GameValidator.java
@@ -1,6 +1,10 @@
 package br.com.gwent.engine.services.validator;
 
 import br.com.gwent.engine.core.GameState;
+import br.com.gwent.engine.exception.NotYourTurnException;
+import br.com.gwent.engine.exception.PlayerDoesNotHaveThisCardOnHandException;
+import br.com.gwent.engine.exception.PlayerHasAlreadyPassedException;
+import br.com.gwent.engine.exception.PlayerNotFoundInGameException;
 import br.com.gwent.engine.pojo.structure.Player;
 
 import java.util.UUID;
@@ -10,33 +14,31 @@ public class GameValidator {
     // players turn? the card he is player it exists on the hand? the player has already passed it?
     public void validatePlayCard (GameState gameState, Long playerId, UUID gameCardId) {
 
+        Player player = gameState.getPlayerById(playerId);
+        if(player == null) throw new PlayerNotFoundInGameException(playerId);
+
         if (!gameState.getCurrentPlayerId().equals(playerId)) {
-            throw new RuntimeException(); //custom exception for not being the player's turn "NotYourTurnException"
+            throw new NotYourTurnException(playerId);
         }
 
-        Player player = gameState.getPlayerById(playerId);
-
-        if(player == null) throw new RuntimeException(); // custom exception for playerNotfoundInTheGame or something like that
-
-        if (player.isHasPassed()) throw new RuntimeException(); // custom exception for "PlayerHasAlreadyPassedException"
+        if (player.isHasPassed()) throw new PlayerHasAlreadyPassedException(playerId);
 
         if (player.getHand().stream()
                 .noneMatch(card -> card.getInstanceId().equals(gameCardId))) {
-            throw new RuntimeException(); // custom exception for "PlayerDoesNotHaveThisCardOnHandException"
+            throw new PlayerDoesNotHaveThisCardOnHandException(playerId, gameCardId);
         }
     }
 
     // players turn? the player has already passed it ?
     public void validatePassTurn (GameState gameState, Long playerId) {
 
+        Player player = gameState.getPlayerById(playerId);
+        if(player == null) throw new PlayerNotFoundInGameException(playerId);
+
         if (!gameState.getCurrentPlayerId().equals(playerId)) {
-            throw new RuntimeException(); //custom exception for not being the player's turn "NotYourTurnException"
+            throw new NotYourTurnException(playerId);
         }
 
-        Player player = gameState.getPlayerById(playerId);
-
-        if(player == null) throw new RuntimeException(); // custom exception for playerNotfoundInTheGame or something like that
-
-        if (player.isHasPassed()) throw new RuntimeException(); // custom exception for "PlayerHasAlreadyPassedException"
+        if (player.isHasPassed()) throw new PlayerHasAlreadyPassedException(playerId);
     }
 }
